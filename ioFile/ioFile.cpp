2dd17e29d98f39a9027ec0cec36366f9bfb05340
foosaan <fauzan4297@gmail.com>
2023-06-21 10:50:03 +0700

    membuat unlimites loop untuk menulis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -13,5 +13,14 @@ int main() {
 
 	cout << ">= Menulis file, \'p\' untuk keluar" << endl;
 
-
+	//unlimites loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
