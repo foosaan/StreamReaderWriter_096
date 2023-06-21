18d2bdd010d781809c52352af09db46202f30bc1
foosaan <fauzan4297@gmail.com>
2023-06-21 11:08:43 +0700

    loop untuk menulis file

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -17,5 +17,15 @@ int main() {
 
 	cout << ">= menulis file, \'q\' untuk keluar" << endl;
 
-	
+	//unlimites loop untuk menulis
+	while (true) {
+		cout << "-";
+		//menambahkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
 }
